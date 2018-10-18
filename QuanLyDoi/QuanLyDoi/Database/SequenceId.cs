13964595d1d4b83666e32f1b28b1091658f37f04
Nông Ngọc Hoài <ngochoaitn@gmail.com>
2018-10-18 11:31:23 +0700

    thêm customgridview và sequence

---
+++ QuanLyDoi/QuanLyDoi/Database/SequenceId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDoi.Database
+{
+    internal class SequenceId
+    {
+        private static QuanLyDoiModel _dbStatic = new QuanLyDoiModel();
+        internal static int Sequence_Dec(string Seq_name, DbContext db)
+        {
+            int seq = (int)(db.Database.SqlQuery<decimal>("SELECT NEXT VALUE FOR " + Seq_name).FirstOrDefault());
+            return seq;
+        }
+    }
+}
