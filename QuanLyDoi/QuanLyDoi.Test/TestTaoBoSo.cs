b53d56e0bd15e2e386cabfa0ea53572a2fd35e6c
Nông Ngọc Hoài <ngochoaitn@gmail.com>
2019-01-04 10:55:18 +0700

    Hoàn thành tạo bộ số

--- QuanLyDoi/QuanLyDoi.Test/TestTaoBoSo.cs
+++ QuanLyDoi/QuanLyDoi.Test/TestTaoBoSo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace QuanLyDoi.Test
@@ -10,9 +11,37 @@ namespace QuanLyDoi.Test
         [TestMethod]
         public void TaoBo4So()
         {
-            List<int> maxVal = new List<int>() { 4, 5, 5, 5 };
+            //List<int> maxVal = new List<int>() { 4, 5, 5, 5 };
+            List<int> maxVal = new List<int>() { 2, 5, 5, 5, 4 };
             new Lib.TaoBoSo(maxVal).TienHanhTao();
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TaoBo4So_WithValidate()
+        {
+            //List<int> maxVal = new List<int>() { 2, 5, 5, 5, 4 };
+            List<int> maxVal = new List<int>() { 3, 2 , 2, 5, 4 };
+            new Lib.TaoBoSo(maxVal).TienHanhTao(validate);
+            Assert.IsTrue(true);
+        }
+
+        private bool validate(List<int> arg)
+        {
+            //Chỉ lấy cái nào 4 số khác 0
+            if (arg.Count(p => p != 0) != 4)
+                return false;
+            //Tổng khác 13 thì loại
+            if (arg.Sum() != 13)
+                return false;
+            //Loại 4 số khác không không liên tiếp
+            //for (int i = 0; i < arg.Count - 4 + 1; i++)
+            //    if (arg[i] != 0
+            //        && arg[i + 1] != 0
+            //        && arg[i + 2] != 0
+            //        && arg[i + 3] != 0)
+            //        return true;
+            return true;
         }
     }
 }
--- QuanLyDoi/QuanLyDoi/Lib/TaoBoSo.cs
+++ QuanLyDoi/QuanLyDoi/Lib/TaoBoSo.cs
@@ -1,44 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuanLyDoi.Lib
 {
     public class TaoBoSo
     {
         List<int> _maxVal, _res;
+        List<List<int>> DanhSachKetQua { get; set; }
         public TaoBoSo(List<int> lst_max_val)
         {
             _maxVal = lst_max_val;
             _res = new List<int>(_maxVal);
+            DanhSachKetQua = new List<List<int>>();
         }
 
-        public void TienHanhTao()
+        public void TienHanhTao(Func<List<int>, bool> validate=null)
         {
-            Tao(0, 0);
+            Tao(0, 0, validate);
         }
-
-        private void Tao(int vi_tri, int gia_tri_hien_tai)
+        
+        private void Tao(int vi_tri, int gia_tri_hien_tai, Func<List<int>, bool> validate = null)
         {
             if(vi_tri < _maxVal.Count)
             {
-                for(int i = 0; i < _maxVal[vi_tri]; i++)
+                for(int i = 0; i <= _maxVal[vi_tri]; i++)
                 {
                     _res[vi_tri] = i;
-                    Tao(vi_tri+1, gia_tri_hien_tai);
+                    Tao(vi_tri+1, gia_tri_hien_tai, validate);
                 }
             }
             else
             {
-                //Tạo đủ 4 số
-                Debug.WriteLine(string.Join(" ", _res));
-            }
-            if(vi_tri < _maxVal.Count && gia_tri_hien_tai < _maxVal[vi_tri])
-            {
-                Tao(vi_tri, gia_tri_hien_tai+1);
+                if (validate?.Invoke(_res) ?? true)
+                {
+                    //Tạo đủ 4 số
+                    Debug.WriteLine(string.Join(" ", _res));
+                    DanhSachKetQua.Add(_res);
+                }
             }
         }
     }
